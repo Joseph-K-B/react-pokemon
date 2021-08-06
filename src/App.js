0936f3311727cd6978b3eae9d8e2318f45902b12
Joseph-K-B <303josephb@gmail.com>
2021-08-06 13:58:31 -0700

    rendering dropdown, not functioning

--- src/App.js
+++ src/App.js
@@ -4,51 +4,73 @@ import PokemonList from './pokemonList';
 // import Dropdown from './dropdown';
 
 class App extends Component {
-  state = { data:[], loading: true, query: null}
+  state = { data:[], loading: true, query: null, order:'asc'}
+
+  componentDidMount() {
+    this.fetchData();
+  };
+
    fetchData = async () => {
+
      let url = 'https://pokedex-alchemy.herokuapp.com/api/pokedex'
+     let searchParams = new URLSearchParams();
+
      if (this.state.query) {
-       url = url + `?pokemon=${this.state.query}`;
-     }
+       searchParams.set('pokemon', this.state.query);
+     };
+     if (this.state.sort) {
+       searchParams.set('sort', 'pokemon');
+       searchParams.set('direction', this.state.order);
+     };
+
+     url= url + `?${searchParams.toString()}`;
+
      let feedback = await fetch(url);
      let data = await feedback.json();
      console.log(data)
      this.setState({ data: data.results, loading:false});
    };
-  componentDidMount() {
-    this.fetchData();
-  };
+
+   sortAscending = (e) => {
+     const { poke } =this.state;
+     poke.sort((a,b) => a - b)
+     this.setState({ poke })
+   }
+
+   sortDescending = (e) => {
+    const { poke } =this.state;
+    poke.sort((a,b) => a - b).reverse()
+    this.setState({ poke })
+  }
+
   queryFeedback = (e) => {
     this.setState ({ query: e.target.value });
   };
+
+  updateOrder = (e) => {
+    this.setState({order: e.target.value})
+  };
+
   render() {
-    // const order = ['ascending', 'descending']
+    //  const order = ('asc, dsc')
     // const type = []
     return (
       <>
         <h2>Pokemon! Gotta catch em all!</h2>
-        {this.state.loading && <p>Walking across a busy road staring at phone screen</p>}
-        {!this.state.loading && (
-          <main>
             <section>
               <div>
               <input onChange={this.queryFeedback} type='text'></input>
               <button onClick={this.fetchData}>Play</button>
-              {/* <Dropdown
-        label='Order:'
-        choices={order}
-        changeEvent={this.keywordChange}
-        />
-        <Dropdown
-        label='Type:'
-        choices={hornChoices}
-        changeEvent={this.hornChange}
-        /> */}
-        </div>
-              <PokemonList pokedex={this.state.data} />
-            </section>
-          </main>
-        )}
+              <select defaultValue={this.state.order} onChange={this.state.updateOrder}>
+                <option value='asc'>Ascending</option>
+                <option value='dsc'>Descending</option>
+                </select>
+                </div>
+                </section>
+                {this.state.loading && <p>Walking across a busy road staring at phone screen</p>}
+                {!this.state.loading && (
+                <PokemonList pokedex={this.state.data} />
+          )}
         </>
     );
   }
--- src/dropdown.js
+++ src/dropdown.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-class Dropdown extends Component {
-    render() {
-        const { changeEvent, choices, label } = this.props;
-        return(
-            <div className='dropdown-div'>
-                <label>{ label }</label>
-                <select className='dropdown' onChange={ changeEvent } />
-                    {choices.map((option, index) => (
-                        <option key={index} value={option}>{option}</option>
-                    ))}
-            </div>
-        )
-    }
-}
-export default Dropdown
+// class Dropdown extends Component {
+//     render() {
+//         const { changeEvent, choices, label } = this.props;
+//         return(
+//             <div className='dropdown-div'>
+//                 <label>{ label }</label>
+//                 <select className='dropdown' onChange={ changeEvent } />
+//                     {choices.map((option) => (
+//                         <option key={option} value={option}>{option}</option>
+//                     ))}
+//             </div>
+//         )
+//     }
+// }
+// export default Dropdown
