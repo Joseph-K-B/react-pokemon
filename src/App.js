6a2c60b427b1ee13975efc9b2af1cfcab0ac4b00
Joseph-K-B <303josephb@gmail.com>
2021-08-06 15:58:08 -0700

    asc/desc dropdown working

--- src/App.js
+++ src/App.js
@@ -4,52 +4,40 @@ import PokemonList from './pokemonList';
 // import Dropdown from './dropdown';
 
 class App extends Component {
-  state = { data:[], loading: true, query: null, order:'asc'}
+  state = { data:[], loading: true, query: null, sortOrder:'asc'}
 
   componentDidMount() {
     this.fetchData();
   };
 
    fetchData = async () => {
-
      let url = 'https://pokedex-alchemy.herokuapp.com/api/pokedex'
      let searchParams = new URLSearchParams();
 
      if (this.state.query) {
        searchParams.set('pokemon', this.state.query);
      };
-     if (this.state.sort) {
+     if (this.state.sortOrder) {
        searchParams.set('sort', 'pokemon');
-       searchParams.set('direction', this.state.order);
+       searchParams.set('direction', this.state.sortOrder);
      };
 
      url= url + `?${searchParams.toString()}`;
+     console.log(url)
 
      let feedback = await fetch(url);
      let data = await feedback.json();
-     console.log(data)
      this.setState({ data: data.results, loading:false});
    };
-
-   sortAscending = (e) => {
-     const { poke } =this.state;
-     poke.sort((a,b) => a - b)
-     this.setState({ poke })
-   }
-
-   sortDescending = (e) => {
-    const { poke } =this.state;
-    poke.sort((a,b) => a - b).reverse()
-    this.setState({ poke })
-  }
 
   queryFeedback = (e) => {
     this.setState ({ query: e.target.value });
   };
 
   updateOrder = (e) => {
-    this.setState({order: e.target.value})
+    this.setState({sortOrder: e.target.value})
   };
+  
 
   render() {
     //  const order = ('asc, dsc')
@@ -59,18 +47,20 @@ class App extends Component {
         <h2>Pokemon! Gotta catch em all!</h2>
             <section>
               <div>
+              <select onChange={this.updateOrder}>
+                <option value='asc'>Ascending</option>
+                <option value='desc'>Descending</option>
+                </select>
               <input onChange={this.queryFeedback} type='text'></input>
               <button onClick={this.fetchData}>Play</button>
-              <select defaultValue={this.state.order} onChange={this.state.updateOrder}>
-                <option value='asc'>Ascending</option>
-                <option value='dsc'>Descending</option>
-                </select>
                 </div>
                 </section>
+                <section>
                 {this.state.loading && <p>Walking across a busy road staring at phone screen</p>}
                 {!this.state.loading && (
                 <PokemonList pokedex={this.state.data} />
-          )}
+                )}
+                </section>
         </>
     );
   }
