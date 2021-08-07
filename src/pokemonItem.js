97e7ed8c9ac0db006503e57f8ecfdb53014093c4
Joseph-K-B <303josephb@gmail.com>
2021-08-07 08:58:44 -0700

    minor changes

--- src/pokemonItem.js
+++ src/pokemonItem.js
@@ -3,7 +3,7 @@ import { Component } from 'react';
 class PokemonItem extends Component {
     render() {
         return(
-                <li>{this.props.choice.pokemon} Type:{this.props.choice.type_1} <img src={this.props.choice.url_image} alt={this.props.choice.pokemon}/></li>
+                <li>Name: {this.props.choice.pokemon} Type: {this.props.choice.type_1} <img src={this.props.choice.url_image} alt={this.props.choice.pokemon}/></li>
         )
     }
 }
