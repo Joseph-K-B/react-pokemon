dced15d03aec5e08a98d605267ed91bf7564bbc3
Joseph-K-B <303josephb@gmail.com>
2021-08-07 08:39:15 -0700

    rendering images

--- src/pokemonItem.js
+++ src/pokemonItem.js
@@ -3,7 +3,7 @@ import { Component } from 'react';
 class PokemonItem extends Component {
     render() {
         return(
-                <li>{this.props.choice.pokemon}</li>
+                <li>{this.props.choice.pokemon} <img src={this.props.choice.url_image} alt={this.props.choice.pokemon}/></li>
         )
     }
 }
