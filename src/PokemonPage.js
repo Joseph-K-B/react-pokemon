c759ee2b8d44c7c3504743234e0c6161815eda61
Joseph-K-B <303josephb@gmail.com>
2021-08-09 14:33:01 -0700

    transfered app.js into PokemonPage.js, added browser router, works when manually entered into URL /pokemon. Need to add header with nav

---
+++ src/PokemonPage.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import PokemonList from './pokemonList.js';
+
+class PokemonPage extends Component {
+    state = { data:[], 
+        loading: true, 
+        query: null, 
+        sortOrder:'asc', 
+        type:'pokemon',
+        page: 1}
+    
+    componentDidMount() {
+        this.fetchData();
+        };
+    
+        fetchData = async () => {
+        let url = 'https://pokedex-alchemy.herokuapp.com/api/pokedex'
+        let searchParams = new URLSearchParams();
+    
+        if (this.state.query) {
+            searchParams.set('pokemon', this.state.query);
+        };
+        if (this.state.sortOrder) {
+            searchParams.set('sort', 'pokemon');
+            searchParams.set('direction', this.state.sortOrder);
+        };
+    url= url + `?${searchParams.toString()}`;
+
+    let feedback = await fetch(url);
+    let data = await feedback.json();
+
+    this.setState({ data: data.results, loading: false});
+    };
+
+    queryFeedback = (e) => {
+        this.setState ({ query: e.target.value });
+      };
+    
+    updateOrder = (e) => {
+    this.setState({sortOrder: e.target.value})
+    };
+
+    pokemonSearch = async () => {
+        await this.setState ({ page: 1 })
+        this.fetchData();
+    }
+    render() {
+        const { loading, feedbackOrder } = this.state;
+        return (
+            <>
+        <h2>Pokemon! Gotta catch em all!</h2>
+            <section class='input'>
+              <div>
+              <select onChange={this.updateOrder}>
+                <option value='asc'>Ascending</option>
+                <option value='desc'>Descending</option>
+                </select>
+              <input onChange={this.queryFeedback} type='text'></input>
+              <button onClick={this.fetchData}>Play</button>
+                </div>
+                </section>
+                <section>
+                {this.state.loading && <p>Walking across a busy road staring at phone screen</p>}
+                {!this.state.loading && (
+                <PokemonList pokedex={this.state.data} />
+                )}
+                </section>
+        </>
+        )
+    }
+}
+ export default PokemonPage
